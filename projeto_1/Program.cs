423ee48d5123722ccaf99e4936fb067a87877838
BrunoRossiGavioli <bruno.gavioli@etec.sp.gov.br>
2020-05-18 22:01:07 -0300

    adicionando o projeto Adivinha Numero

---
+++ projeto_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace projeto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerogerado, numpalpi;
+            string continuar = "s";
+            numerogerado = new Random().Next(1,9);
+
+            while (true)
+            {
+                Console.WriteLine("informe um numero: ");
+                numpalpi = int.Parse(Console.ReadLine());
+                if(numpalpi == numerogerado)
+                {
+                    Console.WriteLine("parabéns você acertou!");
+                }
+                else
+                {
+                    Console.WriteLine("você errou!");
+                }
+
+            }
+        }
+    }
+}
